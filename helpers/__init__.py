ee8f8c8813399ac3a22120a75d0540c3c937c0bb
Prof R.J <43817469+ProfRJ@users.noreply.github.com>
2025-02-05 09:28:21 +1000

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,6 @@
-from .bot_tools import Async_JSON, Checks, Embeds, Downloads, Exceptions, Logger
+from .bot_tools import Async_JSON, Checks, Embeds, Exceptions, Logger
+from .convert_auraflow_checkpoint_to_diffuser import auraflow_checkpoint_to_diffuser
+from .convert_flux_checkpoint_to_diffuser import flux_checkpoint_to_diffuser
 from .convert_vae_pt_to_diffusers import vae_pt_to_vae_diffuser
-from .ctransformer_tools import Ctransformer
-from .diffuser_tools import Diffuser
+from .clip_archiver import CLIP_Archiver
+from .llama_cpp_chat import Llama_Chat
