d9fb62d2942b08554dbbe9f5f82a05903b9ebcfd
Prof R.J <43817469+ProfRJ@users.noreply.github.com>
2025-10-16 08:41:57 +1000

    Update __init__.py

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -4,4 +4,3 @@ from .convert_auraflow_checkpoint_to_diffuser import auraflow_checkpoint_to_diff
 from .convert_flux_checkpoint_to_diffuser import flux_checkpoint_to_diffuser
 from .convert_vae_pt_to_diffusers import vae_pt_to_vae_diffuser
 from .clip_archiver import CLIP_Archiver
-from .llama_cpp_chat import Llama_Chat
